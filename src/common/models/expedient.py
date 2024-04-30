66e7d5d48ed10ed6d75c8c41810228a2e3eec31a
José Souza <joseedsouza.dev@gmail.com>
2024-04-29 23:27:52 -0300

    chore: make Worker model use gen_random_uuid database procedure as default

--- src/common/models/expedient.py
+++ src/common/models/expedient.py
@@ -1,4 +1,5 @@
 from datetime import time
+from uuid import UUID
 
 from sqlalchemy import ForeignKey, Enum, TIME
 from sqlalchemy.orm import Mapped, mapped_column
@@ -10,7 +11,7 @@ from src.db.definitions import Base
 class Expedient(Base):
     __tablename__ = 'expedients'
 
-    worker_id: Mapped[str] = mapped_column(ForeignKey('workers.id'), primary_key=True)
-    week_day: Mapped[str] = mapped_column(Enum(WeekDay, name='week_days'), primary_key=True)
+    worker_id: Mapped[UUID] = mapped_column(ForeignKey('workers.id'), primary_key=True)
+    week_day: Mapped[WeekDay] = mapped_column(Enum(WeekDay, name='week_days'), primary_key=True)
     start_time: Mapped[time] = mapped_column(TIME(timezone=False))
     end_time: Mapped[time] = mapped_column(TIME(timezone=False))
--- src/common/models/worker.py
+++ src/common/models/worker.py
@@ -1,6 +1,6 @@
 from uuid import UUID as _UUID
 
-from sqlalchemy import ForeignKey, UUID, Enum
+from sqlalchemy import ForeignKey, UUID, Enum, text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.common.enums import *
@@ -10,14 +10,18 @@ from src.db.definitions import Base
 class Worker(Base):
     __tablename__ = 'workers'
 
-    id: Mapped[_UUID] = mapped_column(UUID(as_uuid=True), primary_key=True)
+    id: Mapped[_UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        server_default=text('gen_random_uuid()')
+    )
     first_name: Mapped[str]
     last_name: Mapped[str]
-    gender: Mapped[str] = mapped_column(Enum(Gender, name='genders'))
+    gender: Mapped[Gender] = mapped_column(Enum(Gender, name='genders'))
     business_name: Mapped[str]
     job_id: Mapped[int] = mapped_column(ForeignKey('jobs.id'))
     email: Mapped[str] = mapped_column(unique=True)
     phone: Mapped[str] = mapped_column(unique=True)
-    doc_num: Mapped[str] = mapped_column(Enum(DocType, name='doc_types'), unique=True)
-    doc_type: Mapped[str] = mapped_column(Enum(DocType))
+    doc_num: Mapped[str] = mapped_column(unique=True)
+    doc_type: Mapped[DocType] = mapped_column(Enum(DocType, name='doc_types'))
     profile_pic_url: Mapped[str]
