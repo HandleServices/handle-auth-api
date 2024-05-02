ecd39c24b585a4dda966b43d27784a9a0924ba76
José Souza <joseedsouza.dev@gmail.com>
2024-05-02 08:47:57 -0300

    refactor: move some db definitions from definitions.py to __init__.py

--- src/db/definitions.py
+++ src/db/definitions.py
@@ -1,25 +1,10 @@
 from typing import ClassVar
 
 from pydantic import BaseModel
-from sqlalchemy import URL
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm.decl_api import DeclarativeAttributeIntercept
 
 from src.infra.pydantic import to_pydantic
-from src.utils import get_env_var
-
-DATABASE_URL = URL.create(
-    drivername=get_env_var('DB_DRIVER'),
-    username=get_env_var('DB_USER'),
-    database=get_env_var('DB_NAME'),
-    password=get_env_var('DB_PASSWORD'),
-    host=get_env_var('DB_HOST'),
-    port=int(get_env_var('DB_PORT'))
-)
-
-engine = create_async_engine(DATABASE_URL)
-async_session = async_sessionmaker(engine)
 
 
 class BaseMeta(DeclarativeAttributeIntercept):
