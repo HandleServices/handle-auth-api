559329474d576b0b974251fe824b7e454629171e
José Souza <joseedsouza.dev@gmail.com>
2024-05-02 08:47:17 -0300

    chore: add WorkerLogin model

--- src/infra/models/__init__.py
+++ src/infra/models/__init__.py
@@ -2,3 +2,4 @@ from src.infra.models.category import Category
 from src.infra.models.expedient import Expedient
 from src.infra.models.job import Job
 from src.infra.models.worker import Worker
+from src.infra.models.worker_login import WorkerLogin
---
+++ src/infra/models/worker_login.py
@@ -0,0 +1,17 @@
+from uuid import UUID as _UUID
+
+from sqlalchemy import ForeignKey, UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.db.definitions import Base
+
+
+class WorkerLogin(Base):
+    __tablename__ = 'workers_login'
+
+    worker_id: Mapped[_UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey('workers.id'),
+        primary_key=True
+    )
+    hash_password: Mapped[str]
