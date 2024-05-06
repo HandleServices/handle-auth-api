4f195922119c731c8979a0b52910f106ab9f9417
José Souza <joseedsouza.dev@gmail.com>
2024-05-05 23:18:41 -0300

    docs: add verify_existence doc string

--- src/db/utils.py
+++ src/db/utils.py
@@ -24,6 +24,16 @@ async def verify_existence[T, U](
         column: Mapped[U],
         value: U
 ) -> bool:
+    """
+    Verifies if a record exists in the database table based on the given criteria.
+
+    :param session: An asynchronous session object used for executing queries.
+    :param model: The SQLAlchemy model representing the database table.
+    :param column: The column of the model to query against.
+    :param value: The value to match against the column.
+
+    :return: True if a record exists with the provided value in the specified column, False otherwise.
+    """
     query = select(model).where(column == value)
     result = await session.scalar(query)
     return result is not None
